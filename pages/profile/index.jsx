18adaffd1adc72a0cbce1c84def07fe0f2d1e171
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-08-21 19:58:54 +0500

    first

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,11 +1,36 @@
+import { getSession } from 'next-auth/react'
 import React from 'react'
 
-const index = () => {
+const index = ({session}) => {
+  // const {user}=session
+  console.log(session.user.email);
+
   return (
     <>
-      profile
+      profile,
+      <br />{session.user.email}
     </>
   )
 }
 
 export default index
+
+
+export const getServerSideProps = async ({ req }) => {
+
+  const session = await getSession({ req })
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false
+      }
+    }
+
+  }
+  return {
+    props: {
+      session
+    },
+  }
+}
